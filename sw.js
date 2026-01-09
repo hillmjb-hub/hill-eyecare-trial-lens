86c40c6fe36bb091c3e1717d1946bea0a6b2c875
hillmjb-hub <hillmjb@gmail.com>
2026-01-08 20:14:34 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cl-order-pwa-v7";
+const CACHE_NAME = "cl-order-pwa-v7b";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
